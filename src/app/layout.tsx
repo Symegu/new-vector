cbd1272d386bb5eac9cb955dc167751b04688f61
Elize <liza.ves200119@gmail.com>
2025-07-09 18:01:13 +0300

    removed _app & old architecture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 // src/layouts/Layout.tsx
-
+import '@/styles/globals.css';
 // Метаданные страницы (используются в Next.js 13+)
 export const metadata = {
   title: 'Юридическая помощь по банкротству физических лиц',
   description: 'Профессиональная помощь в банкротстве физлиц. Законное списание долгов. Первая консультация бесплатно!',
 };
 
-// Основной layout для всего приложения
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 min-h-screen flex flex-col">
-        {/* Можно вставить глобальные компоненты, например, навигацию */}
+    <html lang="ru" className="dark">
+      <head>
+        {/* Можно добавить дополнительные мета-теги или шрифты */}
+      </head>
+      <body className="bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 min-h-screen flex flex-col">
         {children}
-    </div>
+      </body>
+    </html>
   );
 }
